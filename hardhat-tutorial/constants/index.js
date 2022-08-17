a00ebb4648b2de8a88432753b1b14986a63ca289
Sergey Glibey <sergey.glibey@gmail.com>
2022-08-17 10:58:35 +0300

    -- Web3 DAO dapp

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS = "0x369cfC98b547Ce4668AdE956bDAf73bC4A1d6Cd6";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
